fd127149427dc66193b9d6b47cef48e1732fe2bc
Victor Queja <victorqueja.queja@harman.com>
2025-03-05 17:42:55 +0800

    develop branch

--- src/app/workoutapp/tracker/tracker.component.ts
+++ src/app/workoutapp/tracker/tracker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../footer/footer.component';
@@ -10,18 +10,28 @@ import { Days } from '../common/static/days';
   templateUrl: './tracker.component.html',
   styleUrl: './tracker.component.scss'
 })
-export class TrackerComponent implements OnInit {
+export class TrackerComponent implements OnInit, AfterViewInit {
   editUser: Boolean = false;
   bSeeExercises: Boolean = false;
   days = Days;
-  constructor(){}
+  class = ['card', 'card-today'];
+  constructor(private ref: ChangeDetectorRef){}
 
   ngOnInit(): void {
   }
 
   public getToday(): string {
-    const date = new Date();
+    let date = new Date();
     return this.days[date.getDay()];
+  }
+
+  public setClass(date: string): string {
+    const index = date.toUpperCase() === this.getToday().toUpperCase() ? 1 : 0;
+    return this.class[index];
+  }
+
+  ngAfterViewInit(): void {
+      
   }
 
   
