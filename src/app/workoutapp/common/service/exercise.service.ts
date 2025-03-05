b8d512618968888b84f0fe3f0a58a95ad452a3d3
Victor Queja <victorqueja.queja@harman.com>
2025-03-05 17:41:23 +0800

    added service and json

---
+++ src/app/workoutapp/common/service/exercise.service.ts
@@ -0,0 +1,10 @@
+import { Injectable, Signal, linkedSignal, signal, computed } from '@angular/core';
+import * as exerciseData from '../static/exercise.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExerciseService {
+  // exerciseOptions: Signal<any[]> = [];
+  constructor() { }
+}
